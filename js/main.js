46eed26fb8fd3ad5af746bd3d08531faf0f0bebe
yuHz23 <125856431+yuHz23@users.noreply.github.com>
2024-11-25 18:56:05 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ function firstQuestion() {
         confirmButtonText: CONFIG.btnIntro
     }).then(function() {
         $('.content').show(200);
-        var audio = new Audio('sound/sound.mp3');
+        var audio = new Audio('sound/Mixi.mp3');
         audio.play();
     })
 }
