950e8fe0ee181b994fbae1f037c1f5040725157a
yuHz23 <125856431+yuHz23@users.noreply.github.com>
2024-11-26 09:02:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ const CONFIG = {
     desc: 'Có hay không việc em sẽ khiến 2 vui hơn nhỉ 😉',
     btnYes: 'Đâu làm 2 coi',
     btnNo: 'M cúc liền',
-    question: 'Khi nào unie hong thấy vui thì unie có thể tìm em xả hết chơn mệt nỏi lun',
+    question: 'Khi nào unie hong thấy vui thì unie có thể tìm em xả hết chơn mệt mỏi lun',
     btnReply: 'M biết câu trả lời mà',
     reply: 'Unie hứa',
     mess: 'Em biết unie sẽ nói vậy mà 😗',
-    messDesc: 'Giáng sinh này em hứa sẽ làm unie dui ơi là dui ó',
+    messDesc: 'Em hứa sẽ làm unie dui ơi là dui ó đặc biệt là đên Noel 😃',
     btnAccept: 'Unie chắc chắn đi 😒',
     messLink: 'https://www.messenger.com/t/100081271490586/?locale=vi_VN' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
